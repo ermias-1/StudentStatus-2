dbce0be57767cb2f8e29b9e6123d52a7ac961c2e
ermias-1 <vv8702in@go.minnstate.edu>
2020-02-19 02:39:00 -0600

    Changing the BackColor as well as the ForeColor, and the size of the form to 900x560. Naming the form as Enrollment Status

---
+++ StudentStatus-2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace StudentStatus_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.trkCredits = new System.Windows.Forms.TrackBar();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Credits";
+            // 
+            // trkCredits
+            // 
+            this.trkCredits.Location = new System.Drawing.Point(168, 160);
+            this.trkCredits.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.trkCredits.Maximum = 24;
+            this.trkCredits.Name = "trkCredits";
+            this.trkCredits.Size = new System.Drawing.Size(156, 45);
+            this.trkCredits.TabIndex = 1;
+            this.trkCredits.Scroll += new System.EventHandler(this.trkCredits_Scroll);
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.Location = new System.Drawing.Point(535, 160);
+            this.lblCredits.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(18, 20);
+            this.lblCredits.TabIndex = 2;
+            this.lblCredits.Text = "0";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(486, 374);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(884, 521);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.trkCredits);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Enrollment Status";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trkCredits;
+        private System.Windows.Forms.Label lblCredits;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ StudentStatus-2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentStatus_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trkCredits_Scroll(object sender, EventArgs e)
+        {
+            int credits = trkCredits.Value;
+
+            // Show the Trackbar value in the lblCredits for user feedback
+            lblCredits.Text = trkCredits.Value.ToString();
+
+            // Display the status of the student based on the credits he/she is taking
+            if (credits >= 12)
+            {
+                lblStatus.Text = "Full time";
+            }
+            else if (credits >= 6)
+            {
+                lblStatus.Text = "Half time";
+            }
+            else
+            {
+                lblStatus.Text = "Less than half time";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
